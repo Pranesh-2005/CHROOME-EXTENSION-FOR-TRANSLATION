49b4dce154bc3d06c8f609b60dae891adc76ed82
Pranesh-2005 <150977208+Pranesh-2005@users.noreply.github.com>
2024-04-30 14:46:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from googletrans import Translator, LANGUAGES
+
+app = Flask(__name__)
+CORS(app, resources={r"/add": {"origins": "*"}})
+
+def translate(text, target_language):
+    translator = Translator()
+    translated = translator.translate(text, dest=target_language)
+    return translated.text
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def translate_text():
+    data = request.get_json()
+    x = 'hello how are you'
+    y = 'de'
+    print(translate(x,y))
+
+    
+    if y in LANGUAGES:
+        output = translate(x, y)
+        return jsonify(output=output)
+    else:
+        return jsonify(error="Invalid language code")
+
+if __name__ == "__main__":
+    app.run(debug=True)
